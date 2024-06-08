4fd85b0abdc3cf879563366438f29fc608bbd025
tymeekam <tymeekam@gmail.com>
2024-06-08 14:08:40 -0500

    updated file

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-api_keys = "53fe84b3facffea7703ad2d29b86e214"
+weather_api_key = "53fe84b3facffea7703ad2d29b86e214"
 
 # Geoapify API Key
 geoapify_key = "53fe84b3facffea7703ad2d29b86e214"
